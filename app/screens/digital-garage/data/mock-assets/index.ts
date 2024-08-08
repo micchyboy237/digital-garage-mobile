a5e482669532a6aad3268805170345ab0683212c
Jethro Estrada <jethroestrada237@gmail.com>
2024-08-09 05:42:44 +0800

    Added sample photos

---
+++ app/screens/digital-garage/data/mock-assets/index.ts
@@ -0,0 +1,6 @@
+export const PROFILE_PIC =
+  "https://raw.githubusercontent.com/micchyboy237/digital-garage-mobile/0634c3fab0767cbb111fb43445d3d4ec7848fa7e/app/screens/digital-garage/data/mock-assets/profile-pic.png"
+export const VEHICLE1_PIC =
+  "https://raw.githubusercontent.com/micchyboy237/digital-garage-mobile/0634c3fab0767cbb111fb43445d3d4ec7848fa7e/app/screens/digital-garage/data/mock-assets/vehicle1-photo1.jpeg"
+export const VEHICLE2_PIC =
+  "https://raw.githubusercontent.com/micchyboy237/digital-garage-mobile/0634c3fab0767cbb111fb43445d3d4ec7848fa7e/app/screens/digital-garage/data/mock-assets/vehicle2-photo1.jpeg"
